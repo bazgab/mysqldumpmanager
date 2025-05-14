999b7dc07de8b2d89df4d25bd2a07c557d0aebe8
Gabriel Baz <gabrielbzzt@gmail.com>
2025-05-14 14:42:37 -0300

    <Feat>(Core): added some util functionality for working with files.

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Test")
+	fmt.Println()
+	fmt.Println(os.Getpid())
+	err := os.Mkdir("testdir/anotherdir", 0777)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+
+	}
+
+	//Right now only using this sort of stuff for testing.
+
+	//TODO: clear this up later for actual implementation.
+	if checkIfFileExists("testdir/anotherdir/test.txt") == false {
+		filename := "testdir/anotherdir/test.txt"
+		fmt.Printf("%s: file does not exist\n", filename)
+	} else {
+		fmt.Println("file exists")
+	}
+
+}
+
+// In this section we will just declare some basic util functions to aid file and directory management.
+func checkIfFileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
