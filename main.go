deaff79c4ddad56eca342b30554210f0bd6a499a
Gabriel Baz <gabrielbzzt@gmail.com>
2025-06-11 10:57:02 -0300

    <Refactor>(Core): added utils.go file for general ease of function handling

--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
 
-	fmt.Println()
-	fmt.Println(os.Getpid())
-	fmt.Println()
-
+	fmt.Println("ENTRY POINT: ")
 	c := []string{"mkdir", "testdir"}
 	executeCommand(c)
 	fmt.Println("===================")
+
+	// Insert sample messages for testing
 	loggingInfoForTest := "Info test"
 	loggingWarnForTest := "Warn test"
 	loggingErrorForTest := "Error test"
+
 	logInfo(loggingInfoForTest)
 	logWarn(loggingWarnForTest)
 	logError(loggingErrorForTest)
 	fmt.Println("===================")
+
 	//Checking if deleteFile function will delete directory
-	fmt.Println("Deleting Testdir to not hog tests")
+	fmt.Println("Deleting testdir to not hog tests")
 	deleteFile("testdir")
 	// Yes it will.
 
@@ -45,61 +43,4 @@ func main() {
 		fmt.Println("Found testfile.txt")
 	}*/
 
-}
-
-// TODO: We need to write every logging action to a log file, not just print out the error in the console
-
-// In this section we will just declare some basic util functions to aid file and directory management.
-// Check if file exists
-func checkIfFileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-// Create File:
-// This is going to be used for creating a /etc/mysqldumpmanager file for storing .sql dumps
-// and to create the file for storing MySQL credentials so we don't need to prompt password everytime
-func createFileWithContent(filename string, content string) {
-	f, err := os.Create(filename)
-	if err != nil {
-		logError(err.Error())
-	}
-	_, err = f.WriteString(content)
-	if err != nil {
-		logError(err.Error())
-	}
-}
-
-// Delete file:
-// Used for deleting dump files
-
-func deleteFile(filename string) {
-	err := os.Remove(filename)
-	if err != nil {
-		m := ": File could not be deleted, check if file exists"
-		logError(err.Error() + m)
-	} else {
-		m2 := "File successfully deleted"
-		logInfo(m2)
-	}
-}
-
-// Execute command
-// This will be used to execute the mysqldump command to create new dumps
-func executeCommand(cmd []string) {
-
-	r := exec.Command(cmd[0], cmd[1:]...)
-	logInfo(r.String())
-	err := r.Run()
-	if err != nil {
-		m := ": Command could not be executed, this can happen if the directory is already created on your system"
-		logError(err.Error() + m)
-	}
-	cmdStr := strings.Join(cmd, " ")
-	m := "Executed command: " + cmdStr
-	logInfo(r.String())
-	logInfo(m)
 }
---
+++ utils.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// In this section we will just declare some basic util functions to aid file and directory management.
+// Check if file exists
+func checkIfFileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// Create File:
+// This is going to be used for creating a /etc/mysqldumpmanager file for storing .sql dumps
+// and to create the file for storing MySQL credentials so we don't need to prompt password everytime
+func createFileWithContent(filename string, content string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		logError(err.Error())
+	}
+	_, err = f.WriteString(content)
+	if err != nil {
+		logError(err.Error())
+	}
+}
+
+// Delete file:
+// Used for deleting dump files
+
+func deleteFile(filename string) {
+	err := os.Remove(filename)
+	if err != nil {
+		m := ": File could not be deleted, check if file exists"
+		logError(err.Error() + m)
+	} else {
+		m2 := "File successfully deleted"
+		logInfo(m2)
+	}
+}
+
+// Execute command
+// This will be used to execute the mysqldump command to create new dumps
+func executeCommand(cmd []string) {
+
+	r := exec.Command(cmd[0], cmd[1:]...)
+	err := r.Run()
+	if err != nil {
+		m := ": Command could not be executed, this can happen if the directory is already created on your system"
+		logError(err.Error() + m)
+	}
+	cmdStr := strings.Join(cmd, " ")
+	m := "Executed command successfully: " + "\"" + cmdStr + "\""
+	logInfo(r.String())
+	logInfo(m)
+}
