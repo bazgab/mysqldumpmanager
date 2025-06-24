37d1f2021e4c413db0c99d263f8af7f4409e4fd6
Gabriel Baz <gabrielbzzt@gmail.com>
2025-06-24 18:35:58 -0300

    refactor(core): updates redudant comments

--- cmd/utils.go
+++ cmd/utils.go
@@ -14,18 +14,12 @@ func CheckIfFileExists(filename string) bool {
 	return true
 }
 
-// CreateFileWithContent Create File:
-// This is going to be used for creating a /etc/bum file for storing .sql dumps
-// and to create the file for storing MySQL credentials so we don't need to prompt password everytime
 func CreateFileWithContent(filename string, content []byte) {
 	err := os.WriteFile(filename, content, 0755)
 	if err != nil {
 		LogError(err.Error())
 	}
 }
-
-// Delete file:
-// Used for deleting dump files
 
 func deleteFile(filename string) {
 	err := os.Remove(filename)
@@ -38,8 +32,6 @@ func deleteFile(filename string) {
 	}
 }
 
-// Execute command
-// This will be used to execute the mysqldump command to create new dumps
 func executeCommand(cmd []string) {
 
 	r := exec.Command(cmd[0], cmd[1:]...)
